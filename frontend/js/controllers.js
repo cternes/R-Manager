26f83ded6fc1fb895a9f304c21ceed41236ac705
cternes <github@slackspace.de>
2015-05-10 11:23:07 +0200

    Refactored frontend

---
+++ frontend/js/controllers.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/* Controllers */
+
+var appControllers = angular.module('appControllers', []);
+
+appControllers.controller('LoginController', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
+	
+	$scope.getPlayer = function getPlayer(name) {
+	     $http.get('http://localhost:8080/players/' + name)
+	    .success(function(data, status, headers, config) {
+		localStorage.setItem('playerToken', angular.toJson(data.id));
+		$location.url('/lobby');
+	    })
+	    .error(function(data, status, headers, config) {
+		// todo
+	    });
+	};
+    }]);
+
+appControllers.controller('LobbyController', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
+	
+	var playerToken = angular.fromJson(localStorage.getItem('playerToken'));
+	
+	$http.get('http://localhost:8080/players/' + playerToken + '/matches')
+	    .success(function(data, status, headers, config) {
+		$scope.myMatches = data;
+	    })
+	    .error(function(data, status, headers, config) {
+		// todo
+	    });
+
+	$scope.createMatch = function createMatch() {
+	    $http.post('http://localhost:8080/matches', 'playerToken=' + playerToken)
+		.success(function(data, status, headers, config) {
+		    console.log(data);
+		})
+		.error(function(data, status, headers, config) {
+		    // todo
+		});
+	};
+	
+	$scope.joinRandomMatch = function joinRandomMatch() {
+	    $http.get('http://localhost:8080/players/' + playerToken + '/matches')
+		.success(function(data, status, headers, config) {
+		    
+		})
+		.error(function(data, status, headers, config) {
+		    // todo
+		});
+	};
+    }]);
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,40 +1,27 @@
-//load config file first, then bootstrap angular afterwards
-$.getJSON('js/config/config.json', function(data) {
-    apiKey = data.apiKey;
-    username = data.username;
+'use strict';
 
-    angular.bootstrap(document, ['r-manager']);
-});
+var app = angular.module('r-manager', [
+    'ngRoute',
+    'appControllers'
+]);
 
-var app = angular.module('r-manager', []);
-
-app.run(function($window, $rootScope) {
-    console.log("app run");
-});
-
-app.config(['$httpProvider', function ($httpProvider) {
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
-}]);
-
-app.controller('MatchFindingController', function($scope, $http) {
-
-    $http.get('http://localhost:8080/players/??/matches')
-	    .success(function(data, status, headers, config) {
-		$scope.myMatches = data;
-
-	    })
-	    .error(function(data, status, headers, config) {
-		// todo
-	    });
-
-    $scope.createMatch = function createMatch() {
-	$http.post('http://localhost:8080/matches', 'playerToken=')
-		.success(function(data, status, headers, config) {
-		    console.log(data);
+app.config(['$routeProvider',
+    function($routeProvider) {
+	$routeProvider.
+		when('/', {
+		    templateUrl: 'views/login.html',
+		    controller: 'LoginController'
 		}).
-		error(function(data, status, headers, config) {
-		    // todo
+		when('/lobby', {
+		  templateUrl: 'views/lobby.html',
+		  controller: 'LobbyController'
+		}).
+		otherwise({
+		    redirectTo: '/'
 		});
-    };
+    }]);
 
-});
+app.config(['$httpProvider',
+    function($httpProvider) {
+	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+    }]);
