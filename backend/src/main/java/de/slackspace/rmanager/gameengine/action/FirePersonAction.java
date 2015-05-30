c4aa20022b50ea04680ff265f31f565f6080eb9d
cternes <github@slackspace.de>
2015-05-30 08:39:13 +0200

    Removed inheritance from FirePersonAction

--- backend/src/main/java/de/slackspace/rmanager/gameengine/action/FirePersonAction.java
+++ backend/src/main/java/de/slackspace/rmanager/gameengine/action/FirePersonAction.java
@@ -1,7 +1,10 @@
 package de.slackspace.rmanager.gameengine.action;
 
-public class FirePersonAction extends HirePersonAction {
+public class FirePersonAction implements GameAction {
 
+	private String buildingId;
+	private String personId;
+	
 	protected FirePersonAction() {
 	}
 	
@@ -13,6 +16,22 @@ public class FirePersonAction extends HirePersonAction {
 	@Override
 	public int getType() {
 		return GameAction.FIRE_PERSON;
+	}
+	
+	public String getBuildingId() {
+		return buildingId;
+	}
+
+	public void setBuildingId(String buildingId) {
+		this.buildingId = buildingId;
+	}
+
+	public String getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(String personId) {
+		this.personId = personId;
 	}
 
 }
